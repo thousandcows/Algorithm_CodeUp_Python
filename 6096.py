380c9ca0a28eec217991205e91d4afa92d499e22
thousandcows <milcamboo@gmail.com>
2022-04-09 15:42:32 +0900

    Solved the error: the variable itself cannot change the assigned value in the list

--- 6096.py
+++ 6096.py
@@ -1,15 +1,13 @@
 # 바둑판 초기화
 board = []
 
-for i in range(20):
+for i in range(19):
     board.append([])
-    for j in range(20):
+    for j in range(19):
         board[i].append(0)
 
 for i in range(19):
-    numbers = list(map(int, input().split()))
-    for j in range(19):
-        board[i + 1][j + 1] = numbers[j]
+        board[i] = list(map(int, input().split()))
 
 # 뒤집는 횟수
 n = int(input())
@@ -17,26 +15,24 @@ n = int(input())
 for i in range(n):
 
     # 좌표 입력받기
-    row, col = map(int, input().split())
+    x, y = map(int, input().split())
+
     # 뒤집기
     # row => col
-    for j in range(1, 20):
+    for j in range(19):
 
-        row_stone = board[row][j]
-        col_stone = board[j][col]
-
-        if row_stone == 0:
-            row_stone = 1
+        if board[x-1][j] == 0:
+            board[x-1][j] = 1
         else:
-            row_stone = 0
+            board[x-1][j] = 0
 
-        if col_stone == 0:
-            col_stone = 1
+        if board[j][y-1] == 0:
+            board[j][y-1] = 1
         else:
-            col_stone = 0
+            board[j][y-1] = 0
 
 # 결과 출력
-for i in range(1, 20):
-    for j in range(1, 20):
+for i in range(19):
+    for j in range(19):
         print(board[i][j], end=' ')
     print()
